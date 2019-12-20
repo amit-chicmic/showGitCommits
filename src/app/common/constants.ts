a44935c4cdff76900a029579865af473c4745843
Amit Thakur <tamit9509@gmail.com>
2019-12-20 18:35:32 +0530

    - change records limit to 10

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,3 +1,3 @@
 export const APIS = {
-    GETALLCOMMITS: 'https://api.github.com/repos/amit-chicmic/showGitCommits/commits?page=1&per_page=5'
+    GETALLCOMMITS: 'https://api.github.com/repos/amit-chicmic/showGitCommits/commits?page=1&per_page=10'
 }
