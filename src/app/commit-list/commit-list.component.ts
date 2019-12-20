1311f0c48b95da5fb4afde132a1d35584564e9eb
Amit Thakur <tamit9509@gmail.com>
2019-12-20 15:14:31 +0530

    - list the commit history

--- src/app/commit-list/commit-list.component.ts
+++ src/app/commit-list/commit-list.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import * as moment from 'moment';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from '../services/http.service';
+import { APIS } from '../common/constants';
 @Component({
   selector: 'app-commit-list',
   templateUrl: './commit-list.component.html',
   styleUrls: ['./commit-list.component.css']
 })
 export class CommitListComponent implements OnInit {
-
+  commits: any;
+  title: string;
   constructor(
-    private httpClient: HttpClient
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute,
+
   ) { }
 
   ngOnInit() {
-    this.httpClient.get('https://api.github.com/repos/amit-chicmic/showGitCommits/commits').subscribe(res => {
-      console.log(res);
-
+    this.activatedRoute.data.subscribe((res: any) => {
+      this.title = res.title;
     })
+    this.httpService.getData(APIS.GETALLCOMMITS).subscribe(res => {
+      this.commits = res;
+    })
+  }
+  getDate(str) {
+    return moment(str).format('DD MMM, YYYY')
   }
 
 }
