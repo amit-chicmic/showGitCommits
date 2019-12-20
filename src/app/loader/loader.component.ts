411cec8ce4bf4b62360b16369a15a8c007fb27ce
Amit Thakur <tamit9509@gmail.com>
2019-12-20 18:33:42 +0530

    - add loader component

---
+++ src/app/loader/loader.component.ts
@@ -0,0 +1,22 @@
+//loader.interceptor.ts
+import { Component, OnInit } from '@angular/core';
+import { LoaderService } from '../services/loader.service';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent implements OnInit {
+
+  show: boolean;
+  constructor(private loaderService: LoaderService) {
+    this.loaderService.isLoading.subscribe((value) => {
+      this.show = value;
+    });
+  }
+  ngOnInit() {
+  }
+
+}
+
