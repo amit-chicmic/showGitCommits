adefd9fe21ca7d33de10101dd5056cd1b363b647
Amit Thakur <tamit9509@gmail.com>
2019-12-20 13:47:25 +0530

    - add routes for the commit-list page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommitListComponent } from './commit-list/commit-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'commits', pathMatch: 'full' },
+  { path: 'commits', component: CommitListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
